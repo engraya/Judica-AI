7d15180045b9bc5226a512afda7e58d2ec618b2c
Engraya <ahmadyakubuaya@gmail.com>
2025-02-13 06:34:44 +0100

    Fixed Prod Error

--- src/app/api/bootstrap/route.ts
+++ src/app/api/bootstrap/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 import { initiateBootstrapping } from '../../services/bootstrap';
 
-export const maxDuration = 60;
-
 
 export async function POST() {
   await initiateBootstrapping(process.env.PINECONE_INDEX as string)
